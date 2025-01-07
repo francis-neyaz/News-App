9134e7e57a6d8a6c49e02b32875af10a8cb0080b
maina-git <francismm2023@gmail.com>
2025-01-07 11:20:59 +0300

    first commit

---
+++ vite/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <button className="text-white px-5 py-2 bg-red-500 rounded-lg">Click me </button>
+    </div>
+  )
+}
+
+export default App
