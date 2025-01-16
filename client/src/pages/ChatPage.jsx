986365ca2ce7e4e78343cd235be0931a3690cb8b
maina-git <francismm2023@gmail.com>
2025-01-16 22:20:57 +0300

    "new changes"

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ChatBox from '../components/chats/ChatBox';
+import NewsChatSidebar from '../components/articles/NewsChatSidebar';
 
 
 function ChatPage() {
   return (
     <div className="w-full flex h-screen bg-white">
-      
+      <NewsChatSidebar/>
     </div>
   )
 }
