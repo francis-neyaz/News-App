026cac3a369276d7581e2dfc6f05f47a65644f8c
neyazuddin5050 <neyazuddin5050@gmail.com>
2025-03-28 11:02:17 +0530

    Express/database for login signup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,14 +4,14 @@ import { BiLogoFacebook } from "react-icons/bi";
 
 const Login = () => {
   return (
-    <section className="h-screen bg-black flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
+    <section className="h-screen bg- to-red-600  flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
       <div className="md:w-1/3 max-w-sm">
         <img
           src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
           alt="Sample image"/>
       </div>
       <div className="md:w-1/3 max-w-sm">
-        <div className="text-center md:text-left">
+        <div className="text-center md:text-left text-white">
           <label className="mr-1">Sign in with</label>
           <button
             type="button"
@@ -51,7 +51,7 @@ const Login = () => {
             <span>Remember Me</span>
           </label>
           <a
-            className="text-blue-600 hover:text-blue-700 hover:underline hover:underline-offset-4"
+            className="text-white to-red-500 hover:text-blue-700 hover:underline hover:underline-offset-4"
             href="#"
           >
             Forgot Password?
@@ -59,7 +59,7 @@ const Login = () => {
         </div>
         <div className="text-center md:text-left">
           <button
-            className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
+            className="mt-4 bg-gradient-to-r from-yellow-300 to-red-500 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
             type="submit"
           >
             Login
--- server/config/dotenv.js
+++ server/config/dotenv.js
@@ -4,3 +4,4 @@ module.exports = {
   PORT: process.env.PORT || 5000,
   NEWS_API_KEY: process.env.NEWS_API_KEY,
 };
+
